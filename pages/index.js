198ae9adcd3d45738fff7eff5ef49971935163d1
Grauero <grayero6996@gmail.com>
2018-12-17 18:11:37 +0300

    Update Next js to version 7.0.2

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import Link from 'next/link';
 
 const Index = () => (
   <div>
     <h1>Index page</h1>
-    <Link href="/sell">
-      <a>sell</a>
-    </Link>
   </div>
 );
 
