ba712eede96dd033c658621212821073e3a9c37e
Grauero <grayero6996@gmail.com>
2018-12-24 16:29:23 +0300

    Add pagination split for pages

--- components/items/Items.js
+++ components/items/Items.js
@@ -6,10 +6,11 @@ import styled from 'styled-components';
 
 import Item from './Item';
 import Pagination from '../pagination/Pagination';
+import { perPage } from '../../config';
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int= ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -35,7 +36,13 @@ const ItemsList = styled.div`
 const Items = ({ page }) => (
   <Center>
     <Pagination page={page} />
-    <Query query={ALL_ITEMS_QUERY}>
+    <Query
+      query={ALL_ITEMS_QUERY}
+      variables={{
+        skip: page * perPage - perPage,
+        first: perPage
+      }}
+    >
       {({ data, error, loading }) => {
         if (loading) {
           return <p>Loading...</p>;
