b500924bdb4c250aa74626c0c2a31987686c544f
Grauero <grayero6996@gmail.com>
2018-12-24 17:11:52 +0300

    Create backend part of users registration via passwords/emails + JWT

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,12 @@
-require('dotenv').config({ path: 'variables.env' });
+const cookieParser = require('cookie-parser');
 
+require('dotenv').config({ path: 'variables.env' });
+require('./db');
 const createServer = require('./createServer');
-const db = require('./db');
 
 const server = createServer();
+
+server.express.use(cookieParser());
 
 server.start(
   {
