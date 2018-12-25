6d93dbc1270a8efedcb223a19f86167dc6976678
Grauero <grayero6996@gmail.com>
2018-12-25 16:48:32 +0300

    Add User component for showing current user status

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config({ path: 'variables.env' });
 require('./db');
@@ -7,6 +8,17 @@ const createServer = require('./createServer');
 const server = createServer();
 
 server.express.use(cookieParser());
+// decode JWT to get user ID
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    req.userId = userId;
+  }
+
+  next();
+});
 
 server.start(
   {
