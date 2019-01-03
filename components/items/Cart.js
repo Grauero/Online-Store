dfb195cac178973436e463628c20af1026f6edba
Grauero <grayero6996@gmail.com>
2019-01-03 16:22:56 +0300

    Connect item cart to client local state

--- components/items/Cart.js
+++ components/items/Cart.js
@@ -1,22 +1,47 @@
 import React from 'react';
+import { Query, Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 
 import CartStyles from '../styles/CartStyles';
 import Supreme from '../styles/Supreme';
 import CloseButton from '../styles/CloseButton';
 import Button from '../styles/Button';
 
+const LOCAL_STATE_QUERY = gql`
+  query {
+    cartOpen @client
+  }
+`;
+
+const TOGGLE_CART_MUTATION = gql`
+  mutation {
+    toggleCart @client
+  }
+`;
+
 const Cart = () => (
-  <CartStyles open>
-    <header>
-      <CloseButton title="close">&times;</CloseButton>
-      <Supreme>Your Cart</Supreme>
-      <p>You have __ Items in your cart</p>
-    </header>
-    <footer>
-      <p>111</p>
-      <Button>Checkout</Button>
-    </footer>
-  </CartStyles>
+  <Mutation mutation={TOGGLE_CART_MUTATION}>
+    {toggleCart => (
+      <Query query={LOCAL_STATE_QUERY}>
+        {({ data }) => (
+          <CartStyles open={data.cartOpen}>
+            <header>
+              <CloseButton title="close" onClick={toggleCart}>
+                &times;
+              </CloseButton>
+              <Supreme>Your Cart</Supreme>
+              <p>You have __ Items in your cart</p>
+            </header>
+            <footer>
+              <p>111</p>
+              <Button>Checkout</Button>
+            </footer>
+          </CartStyles>
+        )}
+      </Query>
+    )}
+  </Mutation>
 );
 
 export default Cart;
+export { LOCAL_STATE_QUERY, TOGGLE_CART_MUTATION };
--- components/meta/Nav.js
+++ components/meta/Nav.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Link from 'next/link';
+import { Mutation } from 'react-apollo';
 
 import User from '../auth/User';
 import Signout from '../auth/Signout';
 import NavStyles from '../styles/NavStyles';
+import { TOGGLE_CART_MUTATION } from '../items/Cart';
 
 const Nav = () => (
   <User>
@@ -24,6 +26,13 @@ const Nav = () => (
               <a>Account</a>
             </Link>
             <Signout />
+            <Mutation mutation={TOGGLE_CART_MUTATION}>
+              {toggleCart => (
+                <button type="button" onCLick={toggleCart}>
+                  My Cart
+                </button>
+              )}
+            </Mutation>
           </React.Fragment>
         )}
         {!me && (
